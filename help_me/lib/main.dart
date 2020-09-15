9147bff20704b74fee759c55e98036e387696b0e
Roma Gritsuk <roma.gritsuk@icloud.com>
2020-09-15 12:14:25 +0300

    added localization and folders for the project

--- help_me/lib/main.dart
+++ help_me/lib/main.dart
@@ -1,66 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:help_me/ui/pages/index.dart';
+
+import 'generated/i18n.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final GeneratedLocalizationsDelegate i18n = I18n.delegate;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'HelpMe'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
+        home: MyHomePage(title: 'HelpMe'),
+        localizationsDelegates: [i18n],
+        supportedLocales: i18n.supportedLocales,
+        localeResolutionCallback: i18n.resolution(
+          fallback: const Locale('en', 'US'),
+        ));
   }
 }
