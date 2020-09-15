de1b1b51121eefd391743d753298f0814922ce25
Roma Gritsuk <roma.gritsuk@icloud.com>
2020-09-15 23:18:32 +0300

    added authorization_page

---
+++ help_me/lib/ui/pages/authorization/authorization_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:help_me/generated/i18n.dart';
+
+class AuthorizationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final localization = I18n.of(context);
+  }
+}
