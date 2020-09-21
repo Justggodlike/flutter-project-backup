4bbc2435b0fa747841350ffbed8423cedbabeb64
Justggodlike <sashanik212@gmail.com>
2020-09-21 23:06:42 +0300

    nav bar widget added

--- help_me/lib/ui/pages/shell_page.dart
+++ help_me/lib/ui/pages/shell_page.dart
@@ -3,6 +3,7 @@ import 'package:help_me/core/common/index.dart';
 import 'package:help_me/core/provider_models/index.dart';
 import 'package:help_me/ui/notify_provider_widget.dart';
 import 'package:help_me/ui/pages/index.dart';
+import 'package:help_me/ui/widgets/navbar/index.dart';
 
 class ShellPage extends StatefulWidget {
   @override
@@ -20,7 +21,9 @@ class _ShellPageState extends State<ShellPage> {
             backgroundColor: Colors.blue,
           ),
           backgroundColor: Color.fromARGB(255, 255, 255, 255),
-          body: buildBody(model.selectedPage),
+          body: 
+            buildBody(model.selectedPage),
+            bottomNavigationBar: BottomNavigationBarWidget(),
         );
       },
     );
@@ -35,3 +38,96 @@ class _ShellPageState extends State<ShellPage> {
     }
   }
 }
+/* Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: <Widget> [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(24),
+                  color: Colors.red,
+                  gradient: LinearGradient(
+                    colors: [Colors.red, Colors.pink],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.red,
+                      blurRadius: 12,
+                      offset: Offset(0, 6)
+
+                    ),
+                  ],
+                )
+              ),
+              Positioned.fill(
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(width: 10),
+                    Container(
+                      width: 80,
+                      height: 80,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset('assets/preview.jpg', height: 80, width: 80,),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget> [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Container(
+                              child: Text(
+                                '"Le Italia"',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Container(
+                              child: Text(
+                                'Pizza by very comfortable price.',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              child: Text(
+                                '4.00\$',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 24,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      flex: 4,
+                    ),
+                    SizedBox(width: 30),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      bottomNavigationBar: */
