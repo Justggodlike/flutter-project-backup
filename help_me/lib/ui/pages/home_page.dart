21bafa7463b7a2a3b0efad5be31e6b9abe9e80f3
Roma Gritsuk <roma.gritsuk@icloud.com>
2020-09-26 02:23:17 +0300

    refactor advert items

--- help_me/lib/ui/pages/home_page.dart
+++ help_me/lib/ui/pages/home_page.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:help_me/generated/i18n.dart';
+import 'package:help_me/ui/decorations/assets.dart';
 import 'package:help_me/ui/widgets/advert_list/index.dart';
 
 class HomePage extends StatelessWidget {
+  final List<AdvertItemModel> advertsList = [
+    AdvertItemModel(
+      nameAdvert: 'I want paste here long name and watch what happens',
+      price: '200 p.',
+      imagePath: Assets.avatar,
+      dateOfCreation: '22.09.20 12:43',
+      location: 'Pinsk',
+    ),
+    AdvertItemModel(
+      nameAdvert: 'I want paste here long name and watch what happens',
+      price: '200 p.',
+      imagePath: Assets.avatar,
+      dateOfCreation: '22.09.20 12:43',
+      location: 'Pinsk',
+    ),
+    AdvertItemModel(
+      nameAdvert: 'I want paste here long name and watch what happens',
+      price: '200 p.',
+      imagePath: Assets.avatar,
+      dateOfCreation: '22.09.20 12:43',
+      location: 'Pinsk',
+    ),
+    AdvertItemModel(
+      nameAdvert: 'I want paste here long name and watch what happens',
+      price: '200 p.',
+      imagePath: Assets.avatar,
+      dateOfCreation: '22.09.20 12:43',
+      location: 'Pinsk',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     var localization = I18n.of(context);
-    return Center(
-      child: LazyLoadingPage(), //StandardAdvert(),
+    return AdvertListWidget(
+      itemsList: advertsList,
     );
   }
 }
---
+++ help_me/lib/ui/widgets/advert_list/advert_item_model.dart
@@ -0,0 +1,15 @@
+class AdvertItemModel {
+  final String nameAdvert;
+  final String imagePath;
+  final String location;
+  final String dateOfCreation;
+  final String price;
+
+  AdvertItemModel({
+    this.nameAdvert,
+    this.imagePath,
+    this.location,
+    this.dateOfCreation,
+    this.price,
+  });
+}
